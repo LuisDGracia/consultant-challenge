c05d884e025555aa19d92d483ebedb4d4a3348bb
Zuzki <ldegrac071@hotmail.com>
2021-02-01 09:21:34 -0500

    change

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Main } from './LayoutStyled'
 //Components
 import Body from '../Body/Body'
 import Navbar from '../shared/Navbar/Navbar'
-import Designer from '../Designer/Designer'
 
 export default function Layout() {
 	return (
